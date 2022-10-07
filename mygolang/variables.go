93c0810ee5412c7840e53ea22af2203e1b3907db
Samandar <samandarcoderdev@gmail.com>
2022-10-07 22:31:55 +0500

    variables

---
+++ mygolang/variables.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+const MyFinalVariable int = 4564654
+
+func main() {
+	// normal variable declaring
+	//var name string = "Samandar"
+	// implicit type 
+	var lastname = "Asiydinov"
+	// var age int = 18
+	// var job string = "Android Developer"
+	fmt.Println(lastname)
+
+	// no var style
+	programmingLanguage := "Kotlin"
+	fmt.Println(programmingLanguage)
+
+	fmt.Println(MyFinalVariable)
+}
